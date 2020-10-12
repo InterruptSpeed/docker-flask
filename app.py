0900a331b2331f843c3278a5c20667cd28fdc0a4
Steven Docherty <sdocherty@InterruptSpeed.com>
2020-10-12 22:13:48 +0100

    upgraded docker image to use python3.9.0-alpine3.12 and output python version in app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello world"
+    return sys.version
 
 
 if __name__ == "__main__":
